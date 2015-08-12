b4796674a7f96dc06af9f985c60a98a78f1551d5
Mr_Faton <firefly90@inbox.ru>
2015-08-12 18:53:49 +0300

    home

--- src/main/java/com/mr_faton/SomeTest/Test.java
+++ src/main/java/com/mr_faton/SomeTest/Test.java
@@ -1,20 +1,11 @@
 package com.mr_faton.SomeTest;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
+import java.util.logging.Logger;
 
-/**
- * Created by Mr_Faton on 09.07.2015.
- */
 public class Test {
+    private static final Logger logger = Logger.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
 
-    public static void main(String[] args) throws InterruptedException, SQLException {
-//        String str = "Понравился фильм Терминатор: Генезис. Все очень динамично, не много предсказуемо местами,но я ни разу не заскучала. Хороший фильм и актеры \uD83D\uDC4D";
-        String str = "@Я simple – коралл! Встречайте День Земли: найдите свое животное с помощью викторины #GoogleDoodle.";
-        String upStr = str.replaceAll("[^A-Za-zА-Яа-я0-9\\p{Punct}\\s\\n\\r\\t\\v]", "");
-        System.out.println(upStr);
+    public static void main(String[] args) {
+        System.out.println(logger.getName());
     }
 }
