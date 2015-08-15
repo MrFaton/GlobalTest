e380dfe3be8645c29c23bf197cd3714eb1fcad27
root <firefly90@inbox.ru>
2015-08-15 18:21:30 +0300

    work

--- src/main/java/com/mr_faton/SomeTest/Test.java
+++ src/main/java/com/mr_faton/SomeTest/Test.java
@@ -1,20 +1,19 @@
 package com.mr_faton.SomeTest;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Mr_Faton on 09.07.2015.
  */
 public class Test {
 
-    public static void main(String[] args) throws InterruptedException, SQLException {
-//        String str = "Понравился фильм Терминатор: Генезис. Все очень динамично, не много предсказуемо местами,но я ни разу не заскучала. Хороший фильм и актеры \uD83D\uDC4D";
-        String str = "@Я simple – коралл! Встречайте День Земли: найдите свое животное с помощью викторины #GoogleDoodle.";
-        String upStr = str.replaceAll("[^A-Za-zА-Яа-я0-9\\p{Punct}\\s\\n\\r\\t\\v]", "");
-        System.out.println(upStr);
+    public static void main(String[] args) throws InterruptedException, SQLException, IOException {
+        Properties prop = new Properties();
+        prop.put("JDBC_URL", "jdbc:mysql://127.0.0.1:3306/tweagle?user=Mr_Faton&password=123");
+        prop.store(new FileOutputStream("Settings.properties"), "URL");
     }
 }
