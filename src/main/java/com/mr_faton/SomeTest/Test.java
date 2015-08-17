5dda4aa5fdccf6bc993591be6e36bb3a7c543cea
root <firefly90@inbox.ru>
2015-08-17 08:03:59 +0300

    work

--- src/main/java/com/mr_faton/SomeTest/Test.java
+++ src/main/java/com/mr_faton/SomeTest/Test.java
@@ -1,11 +1,19 @@
 package com.mr_faton.SomeTest;
 
+import java.io.File;
+import java.io.InputStream;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Test {
-    private static final Logger logger = Logger.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
+    private static final String fileName = "ttt.properties";
 
     public static void main(String[] args) {
-        System.out.println(logger.getName());
+        ClassLoader loader = Test.class.getClassLoader();
+        InputStream in = loader.getResourceAsStream(fileName);
+        Scanner scanner = new Scanner(in);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
     }
 }
