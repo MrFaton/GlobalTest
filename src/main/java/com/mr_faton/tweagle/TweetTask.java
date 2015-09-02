5a4c81911235e65ccb9ec3e5a0eb84cea8424578
root <firefly90@inbox.ru>
2015-09-02 07:57:08 +0300

    New changelist

---
+++ src/main/java/com/mr_faton/tweagle/TweetTask.java
@@ -0,0 +1,15 @@
+package com.mr_faton.tweagle;
+
+import java.util.TimerTask;
+
+/**
+ * Created by root on 02.09.2015.
+ */
+public class TweetTask extends TimerTask {
+    @Override
+    public void run() {
+
+    }
+
+
+}
