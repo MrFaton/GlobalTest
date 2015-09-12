57e6890a4731689d2bd5a29a7e10d818c15b74dc
root <firefly90@inbox.ru>
2015-09-12 12:29:06 +0300

    New changelist

---
+++ src/main/java/com/mr_faton/SomeTest/Test2.java
@@ -0,0 +1,16 @@
+package com.mr_faton.SomeTest;
+
+/**
+ * Created by root on 12.09.2015.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println(convertTime(12317000));
+    }
+    private static String convertTime(long originalTime) {
+        long s = (originalTime / 1000) % 60;
+        long m = (originalTime / 1000 / 60) % 60;
+        long h = (originalTime / 1000 / 60 / 60) % 24;
+        return String.format("%02d:%02d:%02d", h,m,s);
+    }
+}
