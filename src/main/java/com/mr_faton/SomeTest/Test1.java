300b59eb329ab56e10ad02c0a36435f0a02c0cc7
root <firefly90@inbox.ru>
2015-09-16 09:12:48 +0300

    New changelist1

--- src/main/java/com/mr_faton/SomeTest/Test1.java
+++ src/main/java/com/mr_faton/SomeTest/Test1.java
@@ -7,43 +7,35 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 /**
  * Created by Mr_Faton on 11.09.2015.
  */
 public class Test1 {
     public static void main(String[] args) throws IOException, InterruptedException {
-        final Player player = new Player();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                player.play();
+        int num = 5;
+        int[] arr = {2, 5, 4, 9, 0, 5, 6, 8};
+        System.out.println("source arr " + Arrays.toString(arr));
+        System.out.println("source arr length " + arr.length);
+        int[] resultArr = delNum(num, arr);
+        System.out.println("result arr " + Arrays.toString(resultArr));
+        System.out.println("result arr length " + resultArr.length);
+    }
+
+    private static int[] delNum(int num, int[] arr) {
+        int[] updatedArr = new int[arr.length];
+        int updatedArrNum = 0;
+        int totalDel = 0;
+        for (int cur : arr) {
+            if (cur == num) {
+                totalDel++;
+                continue;
             }
-        }).start();
-
-        Thread.sleep(20_000);
-        player.stop();
-
-        Thread.sleep(20_000);
-    }
-}
-
-class Player {
-    private static final String FILE = "Alarm.wav";
-    private final AudioStream audioStream;
-
-    public Player() throws IOException {
-
-        InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(FILE);
-        audioStream = new AudioStream(inputStream);
-
-    }
-
-    public void play() {
-        AudioPlayer.player.start(audioStream);
-    }
-
-    public void stop() {
-        AudioPlayer.player.stop(audioStream);
+            updatedArr[updatedArrNum++] = cur;
+        }
+        int[] resultArr = new int[updatedArr.length - totalDel];
+        System.arraycopy(updatedArr, 0, resultArr, 0, updatedArr.length - totalDel);
+        return resultArr;
     }
 }
