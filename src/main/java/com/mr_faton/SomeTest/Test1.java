dfd2fbc630a847a0223bf409b63f95c962637a14
Mr_Faton <firefly90@inbox.ru>
2015-11-26 08:25:20 +0200

    home

--- src/main/java/com/mr_faton/SomeTest/Test1.java
+++ src/main/java/com/mr_faton/SomeTest/Test1.java
@@ -1,12 +1,6 @@
 package com.mr_faton.SomeTest;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Test1 {
-    static String[][] String = {{"String"}};
-    static int i;
     public static void main(String[] args) {
-        System.out.println(i);
     }
 }
